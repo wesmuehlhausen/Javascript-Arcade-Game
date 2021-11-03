50b19516f1c032df5eb856d5197626883d1cf94c
Wesley Muehlhausen <wesmuehlhausen@gmail.com>
2021-11-02 23:29:08 -0700

    score

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -6,19 +6,22 @@ using UnityEngine.UI;
 public class ScoreScript : MonoBehaviour
 {
     public static int scoreValue = 0;
+    public static int total = 0;
     Text score;
-        
+    float timer = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         score = GetComponent<Text>();
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        score.text = "Poopy Text: " + scoreValue;
+
+        timer += Time.deltaTime;
+        total = (int)timer + scoreValue;
+        score.text = "Poopy Text: " + total;
     }
 }
